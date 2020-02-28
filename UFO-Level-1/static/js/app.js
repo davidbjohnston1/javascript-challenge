4401fc3571ce2e9ac4886b5992a65f8c4ac71bba
David Johnston <dbjohnston@zotecpartners.com>
2020-02-27 20:21:49 -0600

    Updated code

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -7,60 +7,50 @@ var tbody = d3.select("tbody");
 // Console.log the UFO data from data.js
 console.log(data);
 
-// Step 1: Loop Through `data` and console.log each UFO report object
+// Loop Through `data` and console.log each UFO report object
 data.forEach(function(UFOReport) {
   console.log(UFOReport);
 });
 
-// Step 2:  Use d3 to append one table row `tr` for each UFO report object
-// Don't worry about adding cells or text yet, just try appending the `tr` elements.
-data.forEach(function(UFOReport) {
-  console.log(UFOReport);
-  var row = tbody.append("tr");
-});
 
-// Step 3:  Use `Object.entries` to console.log each UFO report value
-data.forEach(function(UFOReport) {
-  console.log(UFOReport);
-  var row = tbody.append("tr");
-
-  Object.entries(UFOReport).forEach(function([key, value]) {
-    console.log(key, value);
-  });
-});
-
-// // Step 4: Use d3 to append 1 cell per UFO report value (datetime, city, state, country, shape, durationMinutes, comments)
-// data.forEach(function(UFOReport) {
-//   console.log(UFOReport);
-//   var row = tbody.append("tr");
-
-//   Object.entries(UFOReport).forEach(function([key, value]) {
-//     console.log(key, value);
-//     // Append a cell to the row for each value
-//     // in the UFO report object
-//     var cell = row.append("td");
-//   });
-// });
-
-// Step 5: Use d3 to update each cell's text with
-// UFO report values (datetime, city, state, country, shape, durationMinutes, comments)
-data.forEach(function(UFOReport) {
-  console.log(UFOReport);
-  var row = tbody.append("tr");
-  Object.entries(UFOReport).forEach(function([key, value]) {
-    console.log(key, value);
-    // Append a cell to the row for each value
-    // in the UFO report object
-    var cell = row.append("td");
-    cell.text(value);
-  });
-});
-
-// BONUS: Refactor to use Arrow Functions!
+// Refactor using Arrow Function
 data.forEach((UFOReport) => {
   var row = tbody.append("tr");
   Object.entries(UFOReport).forEach(([key, value]) => {
     var cell = row.append("td");
     cell.text(value);
   });
+});
+
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData;
+  var filteredData = filteredData.filter(UFOData => UFOData.datetime === inputValue);
+
+  console.log(filteredData);
+
+  tbody.html("")
+
+  // Refactor using Arrow Function
+  filteredData.forEach((UFOReport) => {
+  var row = tbody.append("tr");
+  Object.entries(UFOReport).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+
 });
